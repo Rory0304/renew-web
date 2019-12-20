d7df7c18e9640a49206e603cb806d73c56d452a4
Unknown <pyl9983@naver.com>
2019-12-20 11:42:19 +0900

    fix

--- selab/savemain.php
+++ selab/savemain.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
 
-    //$nav = $_POST["nav"];
+    isset($_POST["nav"]) {$nav = $_POST["nav"];}
+    else {$nav = 'top';}
     $font = $_POST["selectFontFamily"];
     $fontsize = $_POST["selectfontsize"];
     $background = $_POST["selectBackground"];
 
-    $_SESSION['nav'] = 'top';
+    $_SESSION['nav'] = $nav;
     $_SESSION['font'] = $font;
     $_SESSION['fontsize'] = $fontsize;
     $_SESSION['bg'] = $background;
